a5bb858cf0004551f996e8fba50ef246ba9038c2
SSP6258 <ssp6258@yahoo.com.tw>
2022-02-14 21:43:20 +0800

    0130 update

--- house_app.py
+++ house_app.py
@@ -1834,7 +1834,7 @@ def fn_chrome_96_workaround():
 def fn_app(page='data'):
     print(f'fn_app() start, page = {page}')
     fn_chrome_96_workaround()
-    st.legacy_caching.clear_cache()
+    # st.legacy_caching.clear_cache()
 
     st.sidebar.header(f'🔍 資訊篩選:\n')
     year_sel = st.sidebar.slider('交易年(民國)', min_value=100, max_value=111, value=(100, 111))
@@ -1847,7 +1847,7 @@ def fn_app(page='data'):
     if not os.path.exists(ml_model):
         os.makedirs(ml_model)
 
-    page = 'eda'
+    # page = 'eda'
     if page == 'eda':
         df = fn_gen_web_init(path)
         df = df[df['交易年'].apply(lambda x: year_sel[0] <= x <= year_sel[1])]
