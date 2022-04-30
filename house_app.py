ae40bf3fa2a439f3ffdd4d4363009fb32d9a2096
SSP6258 <ssp6258@yahoo.com.tw>
2022-05-01 05:20:43 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1730,7 +1730,7 @@ def fn_gen_web_eda(df):
                                     op=0.55,
                                     size='交易量')
 
-    latest_rel = '0401'
+    latest_rel = '0501'
     records = int(df.shape[0] - np.count_nonzero(df['Latest']))
     latest_records = f'版本:{latest_rel} 有 {records}筆'
     city = list(df['city'].unique())
