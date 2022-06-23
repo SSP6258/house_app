45abb947f6b45128c7712457f15ea19b62ee976e
SSP6258 <ssp6258@yahoo.com.tw>
2022-06-23 10:39:48 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -2685,8 +2685,8 @@ def fn_gen_web_intro():
 
     with st.expander('📌 機器學習專案流程介紹'):
         st.write('')
-        st.subheader(
-            "- Medium - Towards Data Science: [Workflow of a Machine Learning project - Ayush Pant](https://towardsdatascience.com/workflow-of-a-machine-learning-project-ec1dba419b94)")
+        st.write(
+            "- 參考自 Medium - Towards Data Science: [Workflow of a Machine Learning project - Ayush Pant](https://towardsdatascience.com/workflow-of-a-machine-learning-project-ec1dba419b94)")
         img_path = os.path.join(dic_of_path['database'], 'ML Flow')
         imgs = ['ML_Flow_2.png', 'ML.png', 'ML_Models.jpeg', 'Train_Test.png',
                 'SU_UNSU.png', 'Confusion.png', 'Clustering_1.png', 'Classification_0.gif',
