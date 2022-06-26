4e6d0eaa90e9af316aa42dfabb92fb3d361cf80b
SSP6258 <ssp6258@yahoo.com.tw>
2022-06-26 11:30:19 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -66,9 +66,9 @@ dic_of_cn_2_en = {'經度': 'longitude',
 
 def fn_show_img(IMG_path, IMG_file, is_sidebar=False, width=None, caption=None):
     png = os.path.join(IMG_path, IMG_file)
-    # img = png if IMG_file.endswith('.gif') else Image.open(png)
+    img = png if IMG_file.endswith('.gif') or IMG_file.startswith('http') else Image.open(png)
 
-    img = Image.open(png) if IMG_file.endswith('.png') or IMG_file.endswith('.jpeg') else png
+    # img = Image.open(png) if IMG_file.endswith('.png') or IMG_file.endswith('.jpeg') else png
 
     if is_sidebar:
         st.sidebar.image(img, width=width, caption=caption)
