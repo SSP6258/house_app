41396f11e4d4ce43f4bf15c151dd1b01c73cebd0
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-06 15:08:26 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1874,7 +1874,9 @@ def fn_gen_web_eda(df):
     st.subheader('🗺️ 建案位置')
     if build_case != '不限':
         st.write(f'- 建案地址: {df_sel["地址"].values[0]}')
-        st.write(f'- 鄰近小學: {df_sel["sku_name"].values[0]} (距離: {df_sel["sku_dist"].values[0]}公尺, 學生人數: {df_sel["sku_109_total"].values[0]})')
+        st.write(f'- 鄰近小學: {df_sel["sku_name"].values[0]} (距離: {int(df_sel["sku_dist"].values[0])}公尺, 學生人數: {int(df_sel["sku_109_total"].values[0])})')
+        st.write(f'- 捷運距離: {int(df_sel["捷運站距離(m)"].values[0])}公尺 ({df_sel["捷運站"].values[0]})')
+        st.write(f'- 通勤時間: {int(df_sel["MRT_Commute_Time_UL"].values[0])}分 (MRT)')
 
     df_sel['每坪單價'] = df_sel['每坪單價(萬)'].apply(lambda x: str(x) + '萬/坪')
 
