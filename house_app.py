1ddfa6d4e7d6dc9363b9c417332fb02b8b750da2
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-06 15:21:20 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1800,11 +1800,13 @@ def fn_gen_web_eda(df):
              int(To.split('年')[-1].split('月')[0]) - int(From.split('年')[-1].split('月')[0]) + 1
 
     st.write('')
-    st.subheader('🗺️ 建案位置')
-    if build_case != '不限':
+    if build_case == '不限':
+        st.subheader('🗺️ 建案位置')
+    else:
+        st.subheader(f'🗺️ 建案 👉 {build_case} 位置')
         st.write(f'- 建案地址: {df_sel["地址"].values[0]}')
         st.write(
-            f'- 鄰近小學: {df_sel["sku_name"].values[0]} (距離: {int(df_sel["sku_dist"].values[0])}公尺, 學生人數: {int(df_sel["sku_109_total"].values[0])})')
+            f'- 鄰近小學: {df_sel["sku_name"].values[0]} (距離: {int(df_sel["sku_dist"].values[0])}公尺, 學生人數: {int(df_sel["sku_109_total"].values[0])}人)')
         st.write(f'- 捷運距離: {int(df_sel["捷運站距離(m)"].values[0])}公尺 ({df_sel["捷運站"].values[0]})')
         st.write(f'- 通勤時間: {int(df_sel["MRT_Commute_Time_UL"].values[0])}分 (MRT)')
 
