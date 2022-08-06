dd8207f235bc0b4533a850305366dbc2f5bc9ac1
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-06 14:00:37 +0800

    0406 update

--- multiapp.py
+++ multiapp.py
@@ -54,7 +54,7 @@ class MultiApp:
         fn_chrome_96_workaround()
         st.set_page_config(page_title="尋找夢想家", page_icon="🏠")
 
-        imgs = ['house_dora.JPG', 'house_dora.jpg', 'house_dora_2.jpg', 'house_dora_88.JPG']
+        imgs = ['house_dora.JPG', 'house_dora.jpg', 'house_dora_2.jpg', 'house_dora_88.jpg']
         fn_show_img(dic_of_path['database'], imgs[3], is_sidebar=True, width=None, caption="~ 女兒蓋了棟別墅送我 💛 ~", rorate=90)
 
         # try:
