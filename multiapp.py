6c67b8c133f0f20bb41d20769625aab8e17c5d61
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-06 13:32:13 +0800

    0406 update

--- multiapp.py
+++ multiapp.py
@@ -55,17 +55,18 @@ class MultiApp:
         st.set_page_config(page_title="尋找夢想家", page_icon="🏠")
 
         try:
-            with st.sidebar:
-                music = {
-                    1: "https://soundcloud.com/audio-library-478708792/leaning-on-the-everlasting-arms-zachariah-hickman-audio-library-free-music?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
-                    2: "https://soundcloud.com/user-443256645/esther-abrami-no-9-esthers?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
-                    3: "https://soundcloud.com/audio_lava/hulu-ukulele?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-                }
-                i = random.randint(1, len(music))
-                st_player(music[i], key=str(datetime.datetime.now()), playing=False, loop=True, volume=0.1, height=220)
+            assert False
+            # with st.sidebar:
+            #     music = {
+            #         1: "https://soundcloud.com/audio-library-478708792/leaning-on-the-everlasting-arms-zachariah-hickman-audio-library-free-music?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+            #         2: "https://soundcloud.com/user-443256645/esther-abrami-no-9-esthers?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+            #         3: "https://soundcloud.com/audio_lava/hulu-ukulele?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+            #     }
+            #     i = random.randint(1, len(music))
+            #     st_player(music[i], key=str(datetime.datetime.now()), playing=False, loop=True, volume=0.1, height=220)
         except:
-            imgs = ['house_dora.JPG', 'house_dora.jpg', 'house_dora_2.jpg']
-            fn_show_img(dic_of_path['database'], imgs[2], is_sidebar=True, width=None, caption="~ by Dora ~")
+            imgs = ['house_dora.JPG', 'house_dora.jpg', 'house_dora_2.jpg', 'house_dora_88.jpg']
+            fn_show_img(dic_of_path['database'], imgs[3], is_sidebar=True, width=None, caption="~ 女兒蓋了棟別墅送我 💖 ~")
 
         st.sidebar.title("👨‍🏫 [Jack.Pan's](https://www.facebook.com/jack.pan.96/) 房市看板 ")
         # st.sidebar.write('- ✉️ ssp6258@yahoo.com.tw')
