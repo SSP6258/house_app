c5d249b5926d8530d4fb91da3d81a53d615d8f3e
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-18 20:21:01 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1131,6 +1131,8 @@ def fn_gen_analysis_statistic(df):
     df = df[df['地下樓層'].apply(lambda x: str(x) != 'nan' and '地' not in str(x))]
     df['地下樓層'] = df['地下樓層'].astype(int)
 
+    df = df[df['容積率(%)'].apply(lambda x: isinstance(x, int))]
+
     fig_bar = make_subplots(rows=2, cols=2, subplot_titles=('交易年', '交易月', '每坪單價(萬)', '總價(萬)'))
     margin = {'l': 0, 'r': 50, 't': 30, 'b': 20}
 
