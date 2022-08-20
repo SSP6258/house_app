8457f5993500c1f5e3f1817b93a30b247c26e4df
SSP6258 <ssp6258@yahoo.com.tw>
2022-08-20 18:54:44 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1933,9 +1933,8 @@ def fn_gen_web_eda(df):
     #
     # st.sidebar.write(f'{len(st.session_state["feature_sel"])}')
     #
-
-    for i in range(5):
-        st.sidebar.write('')
+    # for i in range(5):
+    #     st.sidebar.write('')
 
     house_typ = '預售屋' if len(df['建築完成年月'].unique()) == 1 else '中古屋'
     # city = df['土地位置建物門牌'].apply(lambda x:x.split('市')+'市')
