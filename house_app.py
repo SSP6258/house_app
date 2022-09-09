089a555ed43b094a52e478537fc7f561ef551888
SSP6258 <ssp6258@yahoo.com.tw>
2022-09-09 08:17:34 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -2419,7 +2419,7 @@ def fn_gen_web_eda(df):
                 df = dic_df_show['每坪單價(萬)']
                 v_max, v_min = df.max().max(), df[df > .01].min().min()
                 st.write(f'最高單價 👉 {v_max} (萬/坪) {df[df==v_max].dropna(thresh=2).index.values[0]}')
-                # st.write(f'最低單價 👉 {v_min} (萬/坪) {df[df==v_min].dropna(thresh=2).index.values[0]}')
+                st.write(f'最低單價 👉 {v_min} (萬/坪) ')
                 fn_gen_bc_deals(build_case, dic_df_show, '每坪單價(萬)')
 
             with tab_price_dist:
