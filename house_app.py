cf908e5a5c708b57f82fa9408c7efdbdbe616754
SSP6258 <ssp6258@yahoo.com.tw>
2022-09-13 19:48:59 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -2000,7 +2000,7 @@ def fn_gen_bc_summary(dic_df_show, key):
             str_max = str_max + f'{k}-{f", {k}-".join(v)} '
 
         for k, v in dic_min.items():
-            str_min = str_min + f'{k}-{", ".join(v)} '
+            str_min = str_min + f'{k}-{f", {k}-".join(v)} '
 
         v_min = int(v_min) if '總' in key else v_min
         st.write(f'{dic_summary[key][0]} 👉 {v_max} {dic_summary[key][2]} : {str_max}')
