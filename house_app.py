d782413d391b659aefa219e11348ed37c8964252
SSP6258 <ssp6258@yahoo.com.tw>
2022-09-14 21:11:15 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -1642,7 +1642,8 @@ def fn_gen_analysis(df, latest_records, build_case):
             dict(label='總戶數', values=df_1['總戶數']),
             dict(label='基地面積(坪)', values=df_1['基地面積(坪)']),
             dict(label='地下樓層', values=df_1['地下樓層']),
-            dict(label='建蔽率(%)', values=df_1['建蔽率(%)']),
+            dict(label='完工年度', values=df_1['完工年度']),
+            # dict(label='建蔽率(%)', values=df_1['建蔽率(%)']),
             # dict(label='容積率(%)', values=df_1['容積率(%)']),
             # dict(label='公設比(%)', values=df_1['公設比(%)']),
         ]
