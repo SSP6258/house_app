db59ceb02f859cc2dc314ef8304698af7a90294d
SSP6258 <ssp6258@yahoo.com.tw>
2022-09-18 06:31:13 +0800

    0406 update

--- house_app.py
+++ house_app.py
@@ -883,7 +883,7 @@ def fn_gen_analysis_admin(df, margin=None, bc_name=None):
     df_gb = pd.DataFrame(df_sort.groupby('鄉鎮市區', as_index=True)['每坪單價(萬)'].mean())
     df_gb = df_gb[['每坪單價(萬)']].apply(lambda x: round(x, 2))
 
-    df_sort = df.sort_values(by='每坪單價(萬)', ascending=False)
+    df_gb = df_gb.sort_values(by='每坪單價(萬)', ascending=False)
 
     if bc_name != '不限':
         df_hl = df_sort[df_sort['建案名稱'].apply(lambda x: x in bc_name)]
