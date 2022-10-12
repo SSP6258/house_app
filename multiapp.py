f5dfe0905c038b9a5ce2c14bee2a3389044e35f6
SSP6258 <ssp6258@yahoo.com.tw>
2022-10-12 19:56:21 +0800

    0406 update

--- multiapp.py
+++ multiapp.py
@@ -63,11 +63,11 @@ class MultiApp:
             'house_peace_island_s.jpg': '~ 🏝️ 和平島 ~',
             'house_0919.jpg': '~ 🙏 天佑台灣 ~',
             'house_ocean_1.jpg': '~ 🌊 海闊天空 ~',
-            'house_view.jpg': '~ 🎑 家的視野 ~',
+            'house_view.JPG': '~ 🎑 家的視野 ~',
             'house_green.JPG': '~ 綠光 ~',
         }
 
-        img = 'house_view.jpg'
+        img = 'house_view.JPG'
         fn_show_img(dic_of_path['database'], img, is_sidebar=True, width=None, caption=dic_imgs[img])
 
         # try:
