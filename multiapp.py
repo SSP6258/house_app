bef408207716ff9a6d1f0a396946212638137399
SSP6258 <ssp6258@yahoo.com.tw>
2023-01-08 13:40:33 +0800

    0406 update

--- multiapp.py
+++ multiapp.py
@@ -69,7 +69,7 @@ class MultiApp:
             'house_0108.JPG': '~ 女兒小二時的勞作 ~',
         }
 
-        img = 'house_cloud.JPG'
+        img = 'house_0108.JPG'
         fn_show_img(dic_of_path['database'], img, is_sidebar=True, width=None, caption=dic_imgs[img])
 
         # try:
