e477c1f337b8c483f93de2c18783a0f2c4430785
SSP6258 <ssp6258@yahoo.com.tw>
2023-02-27 10:12:29 +0800

    0406 update

--- multiapp.py
+++ multiapp.py
@@ -72,9 +72,8 @@ class MultiApp:
             'bird.JPG': '~ 合歡山日出 ~',
         }
 
-
         img = 'me.JPG'
-        fn_show_img(dic_of_path['database'], img, is_sidebar=True, width=None, caption=dic_imgs[img])
+        fn_show_img(dic_of_path['database'], img, is_sidebar=True, width=300, caption=dic_imgs[img])
 
         # try:
         #     with st.sidebar:
